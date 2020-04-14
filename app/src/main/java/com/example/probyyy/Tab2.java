5be5e6a26aaa6ac94828239e312f2e6dac2452a1
Dina-ui <dina.shafikova2002@yandex.ru>
2020-04-15 02:35:52 +0500

    Initial commit

---
+++ app/src/main/java/com/example/probyyy/Tab2.java
@@ -0,0 +1,19 @@
+package com.example.probyyy;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class Tab2 extends Fragment {
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.tab2, container, false);
+        return root;
+    }
+}
